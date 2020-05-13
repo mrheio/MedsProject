929f0037ec4d2de98761a6fd08f91ee52b470660
DrgOv <ovidiu.dragoi00@gmail.com>
2020-05-13 21:54:07 +0300

    code cleaned

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,4 @@
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.stage.Stage;
 import misc.users.UserMisc;
 import misc.utility.ViewMisc;
@@ -16,8 +13,9 @@ public class Main extends Application {
         ViewMisc.showStage("/view/menuView/loginView.fxml");
     }
 
-
     public static void main(String[] args) {
         launch(args);
     }
+
+
 }
