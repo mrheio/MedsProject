8fd9ec795cae738a0e4fc83f0688449552a95607
DrgOv <ovidiu.dragoi00@gmail.com>
2020-06-20 13:51:22 +0300

    changes

--- src/main/java/misc/utility/FileMisc.java
+++ src/main/java/misc/utility/FileMisc.java
@@ -1,16 +1,26 @@
 package misc.utility;
 
 import java.io.File;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class FileMisc {
 
-    private static final String projectFolderLocation = System.getProperty("user.dir");
-    private static final Path usersPath = Paths.get(projectFolderLocation, "users.json");
+    private static final String PROJECT_FOLDER = System.getProperty("user.dir");
 
-    public static Path getUsersPath() {
-        return usersPath;
+    public static Path getApplicationPath() {
+        return Paths.get(PROJECT_FOLDER);
+    }
+
+    public static Path getPathToFile(String... path) {
+        return getApplicationPath();
+    }
+
+    public static void ifFileNotFound(String... path) {
+        if (!Files.exists(getApplicationPath())) {
+            getApplicationPath().toFile().mkdirs();
+        }
     }
 
 }
